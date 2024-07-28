6e68b0faa24f15aec81085c39cc0fe604b948605
Prakhar <prakharojha12@gmail.com>
2024-07-29 01:01:18 +0530

    added sidebar icons

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -1,36 +1,81 @@
 // components/Sidebar.tsx
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { CiVideoOn } from "react-icons/ci";
+import { FaFileImport } from "react-icons/fa6";
+import { MdDashboard } from "react-icons/md";
+import { IoBookSharp } from "react-icons/io5";
+import { RiGalleryFill } from "react-icons/ri";
+
 import styles from '@/styles/Sidebar.module.css';
 
-interface MenuItem {
+interface SubMenuItem {
   icon: JSX.Element;
   label: string;
   link: string;
 }
 
+interface MenuItem {
+  icon: JSX.Element;
+  label: string;
+  link?: string;
+  subItems?: SubMenuItem[];
+}
+
 const menuItems: MenuItem[] = [
-  { icon: <span>🏠</span>, label: 'Home', link: '/' },
-  { icon: <span>📁</span>, label: 'Projects', link: '/projects' },
-  { icon: <span>📧</span>, label: 'Contact', link: '/contact' },
+  {
+    icon: <CiVideoOn />,
+    label: 'Studio',
+    subItems: [
+      { icon: <FaFileImport />, label: 'Import', link: '/projects/project1' },
+      { icon: <MdDashboard />, label: 'Dashboard', link: '/projects/project2' },
+    ],
+  },
+  {
+    icon: <IoBookSharp />,
+    label: 'Learner',
+    subItems: [
+      { icon: <RiGalleryFill />, label: 'NFTs', link: '/projects/project2' },
+      { icon: <MdDashboard />, label: 'Dashboard', link: '/projects/project1' },
+    ],
+  },
   // Add more items as needed
 ];
 
 const Sidebar: React.FC = () => {
   const [selected, setSelected] = useState<string>('');
 
+  const handleClick = (label: string) => {
+    setSelected(label);
+  };
+
   return (
     <div className={styles.sidebar}>
       {menuItems.map((item) => (
-        <Link href={item.link} key={item.label}>
+        <div key={item.label} className={styles.menuItem}>
           <div
-            className={`${styles.menuItem} ${selected === item.label ? styles.active : ''}`}
-            onClick={() => setSelected(item.label)}
+            className={`${styles.menuItemLabel} ${selected === item.label ? styles.active : ''}`}
+            onClick={() => handleClick(item.label)}
           >
             <div className={styles.icon}>{item.icon}</div>
             <div className={styles.label}>{item.label}</div>
           </div>
-        </Link>
+          {item.subItems && (
+            <div className={styles.subMenu}>
+              {item.subItems.map((subItem) => (
+                <Link href={subItem.link} key={subItem.label}>
+                  <div
+                    className={`${styles.subMenuItem} ${selected === subItem.label ? styles.active : ''}`}
+                    onClick={() => handleClick(subItem.label)}
+                  >
+                    <div className={styles.icon}>{subItem.icon}</div>
+                    <div className={styles.label}>{subItem.label}</div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
